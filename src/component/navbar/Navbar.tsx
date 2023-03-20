ea5b642c87cefe3150961bbe10ee5d6b67fe9f1b
syedsxjjad <syedsxjjad@gmail.com>
2023-03-21 02:22:37 +0500

    feat: make sticky

--- src/component/navbar/Navbar.tsx
+++ src/component/navbar/Navbar.tsx
@@ -4,19 +4,37 @@
 import React from 'react';
 import { FaMixer, FaStream } from 'react-icons/fa';
 
-import Button from '@/component/Button';
+// import Button from '@/component/Button';
 import MobileScreen from '@/component/navbar/MobileScreen';
 import Navlinks from '@/component/navbar/Navlinks';
 
 const Navbar = () => {
   const [navbarOpen, setNavbarOpen] = React.useState(false);
+  const [sticky, setSticky] = React.useState(false);
 
+  const handleScroll = () => {
+    if (window.scrollY >= 90) {
+      setSticky(true);
+    } else {
+      setSticky(false);
+    }
+  };
+  window.addEventListener('scroll', handleScroll);
+
+  // React.useEffect(() => {
+  //   const handleScroll = () => {
+  //     setSticky(window.scrollY > 200);
+  //   };
+  //   window.addEventListener('scroll', handleScroll);
+  //   return () => window.removeEventListener('scroll', handleScroll);
+  // }, []);
+  // console.log(sticky);
   return (
     <>
       <nav
-        className={`fixed inset-x-0 top-0 z-30 mx-auto flex 
-         justify-between bg-gray-50 p-2 px-4 shadow-md backdrop-blur-md lg:px-10 
-         `}
+        className={`fixed inset-x-0 top-0 z-30 mx-auto flex
+         justify-between bg-gray-50 p-2 px-4 shadow-md backdrop-blur-md transition-all duration-500 ease-in lg:px-10 
+         ${sticky ? '-top-2' : 'top-0'}`}
       >
         <div>
           <h3 className="text-primary-zinc flex items-center px-1 font-sans text-lg font-semibold">
@@ -31,7 +49,7 @@ const Navbar = () => {
           <ul className="active:text-red hidden items-center space-x-12 lg:flex">
             <Navlinks />
           </ul>
-          <Button btnText="Download App" />
+          {/* <Button btnText="Download App" /> */}
           <div className="cursor-pointer">
             {navbarOpen ? (
               <FaMixer
